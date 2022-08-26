e660938117b0c3ee8cf67e85c75d7ed09b4da34f
Tejprakash <67041377+Tej988@users.noreply.github.com>
2022-08-26 15:13:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import AddBlog from "./pages/new/AddBlog";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import BlogDetail from "./pages/new/Blog";
+import Display from "./pages/new/Display";
+import Auth from "./pages/new/Auth";
+
+function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
+  return (
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+              <Route path="addblog" element={<AddBlog/>}/>
+              <Route path="blog" element={<BlogDetail/>}/>
+              <Route path="display" element={<Display/>}/>
+              <Route path="Auth" element={<Auth/>}/>
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
